cc8f56e0ec2bcbae9e2e2ae5afd4df1f992d48e8
LogicAtom <logicatom@gmail.com>
2021-08-30 01:34:03 +0000

    create module.css for MainNavigation

--- ms33/src/App.js
+++ ms33/src/App.js
@@ -6,11 +6,13 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 // Route path, exact  is used to ensure the Switch doesnt stop at just /
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path='/' exact>
           <AllMeetupsPage />
