38f5cc3e5c5680e93dd93a2087a0e381d922d117
LogicAtom <logicatom@gmail.com>
2021-08-30 01:01:40 +0000

    import Switch from react-router-dom

--- ms33/src/App.js
+++ ms33/src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
 
+// Route path, exact=true is used to ensure the Switch doesnt stop at just /
 function App() {
   return (
     <div>
-      <Route path='/'>
-        <AllMeetupsPage />
-      </Route>
-      <Route path='/new-meetup'>
-        <NewMeetupPage />
-      </Route>
-      <Route path='/favorites'>
-        <FavoritesPage />
-      </Route>
+      <Switch>
+        <Route path='/' exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
