2670ad8809a30d0245107c7fdfce29f3e6e67dbb
LogicAtom <64234460+LogicAtom@users.noreply.github.com>
2021-08-30 14:44:39 -0400

    move directories from ms33>ms33 to ms33 (root)

---
+++ src/pages/Favorites.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+function FavoritesPage() {
+    return <div>Favorites Page</div>;
+}
+
+export default FavoritesPage;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+function NewMeetupPage() {
+    return <div>New Meetups Page</div>;
+}
+
+export default NewMeetupPage;
