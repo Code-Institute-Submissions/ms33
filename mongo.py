3ac6320bc2ab344c19b301abb03b4bec90687551
LogicAtom <logicatom@gmail.com>
2021-08-31 05:29:16 +0000

    two dictionaries built inside an array

--- mongo.py
+++ mongo.py
@@ -22,6 +22,16 @@ conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
 
+#Pep8 compliance, splitting onto multiple lines
+new_doc = {
+        title: "Polaris-Road_To_Riches", price: "5",
+        prize: "0..2nd-Chance_Lottery_Special_TicketEntry1-VehicleRaffle",
+        county: "charleston", date: "05/19/2021", address: "Rivriera Dr.",
+        city: "mount_pleasant", retailer: "harris_teeter"
+    }
+
+coll.insert(new_doc)
+
 documents = coll.find()
 
 for doc in documents:
